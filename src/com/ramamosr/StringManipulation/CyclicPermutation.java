34afad8d7b0bab9644c2b0b4c97ba65f3c3826b7
Ramamoorthy <ramamosr@users.noreply.github.com>
2021-12-28 04:37:42 +0530

    Update CyclicPermutation.java

--- src/com/ramamosr/StringManipulation/CyclicPermutation.java
+++ src/com/ramamosr/StringManipulation/CyclicPermutation.java
@@ -100,4 +100,52 @@ Explanation 2:
         System.out.println(cp.solve(A,B));
 
     }
+    
+    public int solveScaler(String A, String B) {
+        // append B to B to tackle cyclic permutations 
+        B += B;
+        int n = A.length(), m = B.length();
+        int lps[] = new int[n];
+        computeLPS(lps, A);
+        int i = 0, l = 0, ans = 0;
+        while (i < m - 1) {
+            if (B.charAt(i) == A.charAt(l)) {
+                i++;
+                l++;
+            }
+            if (l == n) {
+                l = lps[l - 1];
+                ans++;
+            } else if (i < m && B.charAt(i) != A.charAt(l)) {
+                if (l > 0) {
+                    l = lps[l - 1];
+                } else {
+                    i++;
+                }
+            }
+        }
+        return ans;
+    }
+
+    //function used to longest proper suffix array
+    public void computeLPS(int lps[], String s) {
+        int n = s.length();
+        lps[0] = 0;
+        int l = 0, i = 1;
+        while (i < n) {
+            if (s.charAt(i) == s.charAt(l)) {
+                lps[i] = l + 1;
+                i++;
+                l++;
+            } else {
+                if (l > 0) {
+                    l = lps[l - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+    }
+
 }

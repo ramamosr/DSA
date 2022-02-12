b5fab3ce3b153f309db661ce7222e5070c5c8926
Ramamoorthy <ramamosr@users.noreply.github.com>
2022-02-12 22:10:35 +0530

    Create TreePair.java

---
+++ src/com/ramamosr/Trees/TreePair.java
@@ -0,0 +1,9 @@
+public class TreePair {
+        public TreeNode first;
+        public int second;
+
+        public TreePair(TreeNode x, int y) {
+            first = x;
+            second = y;
+        }
+}
